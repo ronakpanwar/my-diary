cd75b10fd0bf769b71281ada4d82c981af13afa6
ronakpanwar <ronakpanwar18@gmail.com>
2024-09-06 01:18:47 +0530

    new updates

--- backend/midlewers/fetchuser.js
+++ backend/midlewers/fetchuser.js
@@ -9,8 +9,8 @@ const fetchuser = (req, res, next) => {
     try {
 
         const data = jwt.verify(token, JWT_SECRET);
-        req.user = data.user;
-        next();
+            req.user = data.user;
+            next();
 
     } catch (error) {
         res.send(401);
